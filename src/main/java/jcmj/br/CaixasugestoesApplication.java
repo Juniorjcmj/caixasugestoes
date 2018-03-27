5c5dbf91024118ef9852340f2a3e6a6dd812bb74
Juniorjcmj <jotacfs2010@hotmail.com>
2018-03-27 15:02:08 -0300

    criação do projeto

---
+++ src/main/java/jcmj/br/CaixasugestoesApplication.java
@@ -0,0 +1,12 @@
+package jcmj.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixasugestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixasugestoesApplication.class, args);
+	}
+}
